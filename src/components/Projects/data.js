03262a2c0ee884d0891518d0976246981dc19cc0
Luca Casasola <lucacasasola82@gmail.com>
2021-09-27 14:10:09 -0300

    add discord bot

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,32 +1,52 @@
-const mainUrl="/videos"
+const mainUrl = "/videos";
 
 const data = [
   {
-    videoUrl: mainUrl + '/HenryFoodDemo.mp4', 
+    videoUrl: mainUrl + "/HenryFoodDemo.mp4",
     title: "Henry Food",
-    description: ["1","2","3","4","5"], 
-    link: 'https://github.com/lucae4238/PI-Food-FT14a', 
-    technologies: ["JavaScript","React", "Redux", "Styled-Components", "PostgreSQL", "Sequelize", "NodeJS & Express", "CSS"]
-
-  }, 
+    description: ["1", "2", "3", "4", "5"],
+    link: "https://github.com/lucae4238/PI-Food-FT14a",
+    technologies: [
+      "JavaScript",
+      "React",
+      "Redux",
+      "Styled-Components",
+      "PostgreSQL",
+      "Sequelize",
+      "NodeJS & Express",
+      "CSS",
+    ],
+  },
   {
-    videoUrl: mainUrl + '/GetNewsDemo.mp4',
+    videoUrl: mainUrl + "/GetNewsDemo.mp4",
     title: "Get News!",
-    description: ["1","2","3"], 
-    link: 'https://github.com/lucae4238/GetNews',
-    technologies: ["React", "Redux", "Material UI", "TypeScript", "CSS"]
-
-  }, 
+    description: ["1", "2", "3"],
+    link: "https://github.com/lucae4238/GetNews",
+    technologies: ["React", "Redux", "Material UI", "TypeScript", "CSS"],
+  },
   {
     imgUrl: mainUrl + "/Nala.png",
-    title: "Nala Rocks", 
-    description: ["1","2","3"],
-    technologies: ["Git","JavaScript","React", "Redux", "Styled-Components", "Sass", "Jest", "Material UI" , "CSS"]
-  }
-]
+    title: "Nala Rocks",
+    description: ["1", "2", "3"],
+    technologies: [
+      "Git",
+      "JavaScript",
+      "React",
+      "Redux",
+      "Styled-Components",
+      "Sass",
+      "Jest",
+      "Material UI",
+      "CSS",
+    ],
+  },
+  {
+    videoUrl: mainUrl + "/bot_demo.mp4",
+    title: "Discord Bot",
+    description: ["1", "2", "3", "4"],
+    link: "https://discord.com/api/oauth2/authorize?client_id=870882811895218207&permissions=0&scope=bot",
+    technologies: ["JavaScript", "NodeJS & Express"],
+  },
+];
 
-export default data
-
-
-
-
+export default data;
